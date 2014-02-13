dc65711896be697d2d2fee1fc6fe7c66521fb648
Luis Pabon <lpabon@redhat.com>
2014-02-12 22:02:01 -0500

    Ok, now it has cleaned up config files

--- autogen.sh
+++ autogen.sh
@@ -12,25 +12,24 @@
 # Here's a command you can run to see what files aclocal will import:
 #  aclocal -I ../autoconf --output=- | sed -n 's/^m4_include..\([^]]*\).*/\1/p'
 
-set -ex
 rm -rf autom4te.cache
 
 trap 'rm -f aclocal.m4.tmp' EXIT
 
 # Use version 1.9 of aclocal and automake if available.
 ACLOCAL=aclocal-1.9
-if test -z `which "$ACLOCAL"`; then
+if ! env "$ACLOCAL" --version > /dev/null 2>&1 ; then
   ACLOCAL=aclocal
 fi
 
 AUTOMAKE=automake-1.9
-if test -z `which "$AUTOMAKE"`; then
+if ! env "$AUTOMAKE" --version > /dev/null 2>&1 ; then
   AUTOMAKE=automake
 fi
 
 # glibtoolize is used for Mac OS X
 LIBTOOLIZE=libtoolize
-if test -z `which "$LIBTOOLIZE"`; then
+if ! env "$LIBTOOLIZE" --version > /dev/null 2>&1 ; then
   LIBTOOLIZE=glibtoolize
 fi
 
@@ -38,18 +37,23 @@ fi
 # changed, which is annoying when the file is not open for edit (in
 # p4).  We work around this by writing to a temp file and just
 # updating the timestamp if the file hasn't change.
+echo "Running $ACLOCAL ..."
 "$ACLOCAL" --force -I m4 --output=aclocal.m4.tmp
-if cmp aclocal.m4.tmp aclocal.m4; then
+if cmp aclocal.m4.tmp aclocal.m4 > /dev/null 2>&1 ; then
   touch aclocal.m4               # pretend that we regenerated the file
   rm -f aclocal.m4.tmp
 else
   mv aclocal.m4.tmp aclocal.m4   # we did set -e above, so we die if this fails
 fi
 
-grep -q LIBTOOL configure.ac && "$LIBTOOLIZE" -c -f
-autoconf -f -W all,no-obsolete
+echo "Running autoheader ..."
 autoheader -f -W all
-"$AUTOMAKE" -a -c -f -W all
+echo "Running $LIBTOOLIZE ..."
+"$LIBTOOLIZE" --automake -c -f
+echo "Running autoconf ..."
+autoconf -f -W all,no-obsolete
+echo "Running automake ..."
+"$AUTOMAKE" -a -c -f --foreign -W all
 
 rm -rf autom4te.cache
 exit 0
