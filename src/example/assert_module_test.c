52dcebba053de09e3f94199c17d55e163e960b9c
RocFang <fangpeng1986@gmail.com>
2015-01-20 18:10:32 +0800

    Update assert_module_test.c
    add stdint.h for compile problem.

--- src/example/assert_module_test.c
+++ src/example/assert_module_test.c
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <stdint.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
